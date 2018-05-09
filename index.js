b8353210f47a9c853aecf37fecf9d098b352b84b
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-05-09 23:29:01 +0200

    adapt script to take needed parameters

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const chromium = require('./lib/chromium');
-const sandbox = require('./lib/sandbox');
-const log = require('lambda-log');
-const apiHandler = require('./lib/api-handler');
+const chromium = require("./lib/chromium");
+const sandbox = require("./lib/sandbox");
+const log = require("lambda-log");
+const apiHandler = require("./lib/api-handler");
 
 if (process.env.DEBUG_ENV || process.env.SAM_LOCAL) {
   log.config.debug = true;
   log.config.dev = true;
 }
 
-log.info('Loading function');
+log.info("Loading function");
 
 // Create new reusable session (spawns chromium and webdriver)
 if (!process.env.CLEAN_SESSIONS) {
@@ -21,25 +21,34 @@ exports.postApiGatewayHandler = apiHandler;
 
 // Default function event handler
 // Accepts events:
-// * {"Base64Script": "<<encoded selenium script>>"}
-// * {"pageUrl": "<<URI to visit>>"}
-// Accepts environment variables:
-// * BASE64_SCRIPT: encoded selenium script
-// * PAGE_URL: URI to visit
+// * {"firstName": "", lastName: "", bookAppointent, }
+
 exports.handler = (event, context, callback) => {
   $browser = sandbox.initBrowser(event, context);
 
   var opts = sandbox.buildOptions(event, $browser);
-  sandbox.executeScript(opts, function (err, result) {
+  const result = sandbox.executeScript(opts);
+
+
+  result.then((result) => {
     if (process.env.LOG_DEBUG) {
-      log.debug(child.execSync('ps aux').toString());
-      log.debug(child.execSync('cat /tmp/chromedriver.log').toString())
+      log.debug(child.execSync("ps aux").toString());
+      log.debug(child.execSync("cat /tmp/chromedriver.log").toString());
     }
-    if (err) {
+
+
+    callback(null, {
+      successfullyBookedAppointment: true,
+      message: "Finished executing script"
+    });
+  }).catch((err) => {
+      if (process.env.LOG_DEBUG) {
+        log.debug(child.execSync("ps aux").toString());
+        log.debug(child.execSync("cat /tmp/chromedriver.log").toString());
+      }
+
       log.error(err);
       return callback(err, null);
     }
-
-    callback(null, 'Finished executing script');
-  });
+  );
 };
